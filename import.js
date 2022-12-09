9215dfd6ff1e63c33e53b985d28eb714397d1fca
anonymouswalker <hoanganhtran1998@gmail.com>
2022-12-08 21:59:25 -0600

    wait for all imports complete

--- import.js
+++ import.js
@@ -1,6 +1,11 @@
 const quickDraw = require('quickdraw.js');
 
 
+const tasks = []
 const objects = ['circle', 'triangle', 'square', 'star'];
-objects.forEach(o => quickDraw.import(o, 2000))
+objects.forEach(o => { 
+    tasks.push(quickDraw.import(o, 2000))
+})
+
+Promise.all(tasks).then(() => console.log("Import complete"))
 
