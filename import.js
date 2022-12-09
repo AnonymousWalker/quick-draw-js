5c62f7b9b4bfa11db122fca473d7c8bb11fa04ca
anonymouswalker <hoanganhtran1998@gmail.com>
2022-12-08 21:14:28 -0600

    import before start

--- import.js
+++ import.js
@@ -2,6 +2,6 @@ const quickDraw = require('quickdraw.js');
 
 
 const objects = ['circle', 'triangle', 'square', 'hexagon', 'star'];
-objects.forEach(o => quickDraw.import(o, 1000))
+objects.forEach(o => quickDraw.import(o, 2000))
 // console.log(quickDraw.checkSet('motorbike'))
 
